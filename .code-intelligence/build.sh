75013a836ce796a66bc06fe59f45ebf32bb0470e
Khaled Yakdan <yakdan@code-intelligence.com>
2022-03-02 17:19:32 +0100

    Initial CMake support

--- .code-intelligence/build.sh
+++ .code-intelligence/build.sh
@@ -3,6 +3,6 @@ set -e
 mkdir -p $WORK_DIR
 cd $WORK_DIR
 
-cmake -DCI_FUZZ_INSTALL_ROOT=/opt/ci-fuzz-2.22.0 -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
+cmake -DCIFUZZ_INSTALL_ROOT=/opt/ci-fuzz-2.29.1 -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
 
 make -j$(nproc)
